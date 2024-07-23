e0f2068f3f0fc709dc84ae9047d227a37a220318
Antoine D <antoine.delenclos37@gmail.com>
2024-07-24 00:51:54 +0900

    Changes for chromosome training

--- P1.py
+++ P1.py
@@ -6,70 +6,30 @@ class Player1AI():
     def __init__(self, chromosome):
         self.chromosome = chromosome
     def get_move(self, game):
-        legal_moves = game.get_legal_moves()
-        #you can retrieve information from the game object
-        print("remaining walls",game.walls)
-        # print("player_positions",game.player_positions)
-        # print("board",game.board) 
-        # ("P1",legal_moves)
-        # print("P1 start",game.player_positions.get('P1'))
-        start = game.player_positions.get('P1')
-        goals = [(0,i) for i in range(5)]
-        # goals = [(0,0)]
-        grid = [[0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0]]
-        walls = transformBoardToWallsTuple(game.board)
-        path = bfs(start,goals,grid,walls)
-        move_input = input_to_reach_next(start,path[1])
-        print("Start:",start)
-        print("Goal:",goals)
-        print("Walls:",walls)
-        print("PATH",path)
-        print("Move_input:",move_input)
+        # legal_moves = game.get_legal_moves()
+        # #you can retrieve information from the game object
+        # print("remaining walls",game.walls)
+        # # print("player_positions",game.player_positions)
+        # # print("board",game.board) 
+        # # ("P1",legal_moves)
+        # # print("P1 start",game.player_positions.get('P1'))
+        # start = game.player_positions.get('P1')
+        # goals = [(0,i) for i in range(5)]
+        # # goals = [(0,0)]
+        # grid = [[0, 0, 0, 0, 0],
+        #         [0, 0, 0, 0, 0],
+        #         [0, 0, 0, 0, 0],
+        #         [0, 0, 0, 0, 0],
+        #         [0, 0, 0, 0, 0]]
+        # walls = transformBoardToWallsTuple(game.board)
+        # path = bfs(start,goals,grid,walls)
+        # move_input = input_to_reach_next(start,path[1])
+        # print("Start:",start)
+        # print("Goal:",goals)
+        # print("Walls:",walls)
+        # print("PATH",path)
+        # print("Move_input:",move_input)
 
-        action = strategy([2,2,1],1,game)
-        return((action,))
-    
-    #wallfunc
-    def place_wall(self, game, wall_type, row, col):
-        """
-        Attempt to place a wall for Player 1.
-        return:
-        bool: True if wall placed
-        """
-        # check walls left
-        if game.walls['P1'] <= 0:
-            return False
-        
-        # check coordo in valid range
-        if row < 0 or row >= game.board_size - 1 or col < 0 or col >= game.board_size - 1:
-            return False
-        
-        # if wall isn't offboard
-        if (wall_type == 'H' and col == game.board_size - 2) or (wall_type == 'V' and row == game.board_size - 2):
-            return False
-        
-        # if space taken by a wall
-        if game.board[row][col] != False or game.board[row][col+1] != False or game.board[row+1][col] != False:
-            return False
-        
-        # place it temp
-        previous = game.update_board_wall((wall_type, row, col))
-        
-        # check if p1 and p2 can reach
-        p1_can_reach = game.reachable(game.player_positions['P1'], (0, 0))
-        p2_can_reach = game.reachable(game.player_positions['P2'], (game.board_size-1, 0))
-        
-        if not (p1_can_reach and p2_can_reach):
-            # revert cuz those dumasses cannot reach
-            game.restore_board_wall((wall_type, row, col), previous)
-            return False
-        
-        # valid placement
-        return True
-
-
-       
+        # action = strategy([2,2,1],1,game)
+        action = strategy(chromosome,1,game)
+        return((action,))       
--- P2.py
+++ P2.py
@@ -5,19 +5,20 @@ class Player2AI:
     def __init__(self, chromosome):
         self.chromosome = chromosome
     def get_move(self, game):
-        legal_moves = game.get_legal_moves()
-        # print("P2",legal_moves)
-        start = game.player_positions.get('P2')
-        goals = [(4,i) for i in range(5)]
-        # goals = [(4,2)]
-        grid = [[0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0]]
-        walls = transformBoardToWallsTuple(game.board)
-        # print("WALLS :",walls)
-        path = bfs(start,goals,grid,walls)
-        move_input = input_to_reach_next(start,path[1])
-        # print("PATH",path)
-        return((move_input,))
+        # legal_moves = game.get_legal_moves()
+        # # print("P2",legal_moves)
+        # start = game.player_positions.get('P2')
+        # goals = [(4,i) for i in range(5)]
+        # # goals = [(4,2)]
+        # grid = [[0, 0, 0, 0, 0],
+        #         [0, 0, 0, 0, 0],
+        #         [0, 0, 0, 0, 0],
+        #         [0, 0, 0, 0, 0],
+        #         [0, 0, 0, 0, 0]]
+        # walls = transformBoardToWallsTuple(game.board)
+        # # print("WALLS :",walls)
+        # path = bfs(start,goals,grid,walls)
+        # move_input = input_to_reach_next(start,path[1])
+        # # print("PATH",path)
+        action = strategy(chromosome,1,game)
+        return((action,))
